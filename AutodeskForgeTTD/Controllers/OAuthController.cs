eb30c4843b306200bd4361621b2d9e62488a3385
PhamHoan <phamhoanxda@gmail.com>
2021-02-26 15:01:28 +0700

    Add project files.

---
+++ AutodeskForgeTTD/Controllers/OAuthController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Autodesk.Forge;
+
+namespace AutodeskForgeTTD.Controllers
+{
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {  
+        // As both internal & public tokens are used for all visitors
+        // we don't need to request a new token on every request, so let's
+        // cache them using static variables. Note we still need to refresh
+        // them after the expires_in time (in seconds)
+
+        private static dynamic InternalToken { get; set;}
+        private static dynamic PublicToken { get; set; }
+
+        /// <summary>
+        /// Get access token with public (viewables:read) scope
+        /// </summary>
+        /// 
+
+        [HttpGet]
+        [Route("api/forge/oauth/token")]
+        public async Task<dynamic> GetPublicAsync()
+        {
+            if(PublicToken == null || PublicToken.ExpiresAt < DateTime.UtcNow)
+            {
+                PublicToken = await Get2LeggedTokenAsync(new Scope[] { Scope.ViewablesRead });
+                PublicToken.ExpiresAt = DateTime.UtcNow.AddSeconds(PublicToken.expires_in);
+            }
+            return PublicToken;
+        }
+
+        public static async Task<dynamic> GetInternalAsync()
+        {
+            if(InternalToken == null || InternalToken.ExpiresAt < DateTime.UtcNow)
+            {
+                InternalToken = await Get2LeggedTokenAsync(new Scope[] { Scope.BucketCreate, Scope.BucketRead, Scope.BucketDelete, Scope.DataRead, Scope.DataCreate, Scope.DataWrite, Scope.CodeAll });
+                InternalToken.ExpireAt = DateTime.UtcNow.AddSeconds(InternalToken.expires_in);
+            }
+            return InternalToken;
+
+        }
+
+        private static string clientId = "FORGE_CLIENT_ID";
+        private static string secret = "FORGE_CLIENT_SECRET";
+
+
+        private static async Task<dynamic> Get2LeggedTokenAsync (Scope[] scopes)
+        {
+            TwoLeggedApi oauth = new TwoLeggedApi();
+            string grantType = "client_cridentials";
+            dynamic bearer = await oauth.AuthenticateAsync(GetAppSetting(clientId), GetAppSetting(secret), grantType, scopes);
+            return bearer;
+
+        }
+
+        public static string GetAppSetting(string settingKey)
+        {
+            return Environment.GetEnvironmentVariable(settingKey).Trim();
+        }
+    }
+}
---
+++ AutodeskForgeTTD/Controllers/OSSController.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.Forge;
+using Autodesk.Forge.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AutodeskForgeTTD.Controllers
+{
+    public class OSSController : ControllerBase
+    {
+        private IWebHostEnvironment _env;
+        public OSSController(IWebHostEnvironment env) { _env = env; }
+        public string ClientId { get { return OAuthController.GetAppSetting("FORGE_CLIENT_ID").ToLower(); } }
+
+        [HttpGet]
+        [Route("api/forge/oss/buckets")]
+        public async Task<IList<TreeNode>>GetOSSAsync(string id)
+        {
+            IList<TreeNode> nodes = new List<TreeNode>();
+            dynamic oauth = await OAuthController.GetInternalAsync();
+
+            if (id == "#") //root
+            {
+                // in this case, let 's return all buckets
+                BucketsApi appBuckets = new BucketsApi();
+                appBuckets.Configuration.AccessToken = oauth.access_token;
+
+                // to simplify, let's return only the first 100 buckets
+                dynamic buckets = await appBuckets.GetBucketsAsync("US", 100);
+                foreach (KeyValuePair<string, dynamic> bucket in new DynamicDictionaryItems(buckets.items))
+                {
+                    nodes.Add(new TreeNode(bucket.Value.bucketKey, bucket.Value.bucketKey.Replace(ClientId + "-", string.Empty), "bucket", true));
+                }
+
+            }
+
+        }
+
+
+    /// <summary>
+    /// Model data for jsTree used on GetOSSAsync
+    /// </summary>
+    public class TreeNode
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+        public bool children { get; set; }
+
+        public TreeNode(string id, string text, string type, bool children)
+        {
+            this.id = id;
+            this.text = text;
+            this.type = type;
+            this.children = children;
+        }
+    }
+}
